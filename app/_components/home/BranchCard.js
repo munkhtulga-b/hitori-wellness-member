3efd76ee9cbdce8c68bea1574df69029c4f7d536
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-02-26 19:02:07 +0800

    auth check, branch card component

---
+++ app/_components/home/BranchCard.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+const BranchCard = () => {
+  return (
+    <div className="tw-p-2 tw-rounded-[16px] tw-shadow tw-w-fit">
+      <div className="tw-flex tw-flex-col tw-gap-2">
+        <section className="tw-rounded-xl tw-min-h-[190px] tw-max-h-[190px] tw-min-w-[220px] tw-max-w-[220px] tw-overflow-hidden tw-bg-gray-200"></section>
+        <section className="tw-flex tw-flex-col tw-gap-1 tw-max-w-[220px]">
+          <span className="tw-text-lg">KARADA BESTA Ginza store</span>
+          <div className="tw-flex tw-flex-col tw-gap-[2px]">
+            <section className="tw-flex tw-justify-start tw-items-center tw-gap-1">
+              <Image
+                src="/assets/branch/location-icon.svg"
+                alt="location"
+                width={20}
+                height={20}
+              />
+              <span className="tw-tracking-[0.14px]">位置</span>
+            </section>
+            <section>
+              <p className="tw-text-sm tw-tracking-[0.12px]">
+                104-0061 東京都中央区築地1-10-11 RATIO広路701
+              </p>
+            </section>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default BranchCard;
--- app/auth/layout.js
+++ app/auth/layout.js
@@ -4,27 +4,38 @@ import Cookies from "js-cookie";
 import MainHeader from "../_components/auth/MainHeader";
 import { motion } from "framer-motion";
 import { usePathname, redirect } from "next/navigation";
+import { Suspense, useLayoutEffect, useState } from "react";
 
 export default function AuthLayout({ children }) {
-  const token = Cookies.get("token");
+  const [isMounted, setIsMounted] = useState(false);
 
-  if (token) {
-    redirect("/");
-  }
+  useLayoutEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      redirect("/home");
+    }
+    setIsMounted(true);
+  }, []);
 
   const pathName = usePathname();
 
   return (
-    <main className="tw-flex tw-flex-col">
-      <MainHeader />
-      <motion.div
-        key={pathName}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        {children}
-      </motion.div>
-    </main>
+    <div className="tw-flex tw-flex-col">
+      {isMounted ? (
+        <>
+          <Suspense fallback={<></>}>
+            <MainHeader />
+          </Suspense>
+          <motion.div
+            key={pathName}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Suspense fallback={<></>}>{children}</Suspense>
+          </motion.div>
+        </>
+      ) : null}
+    </div>
   );
 }
