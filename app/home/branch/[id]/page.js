f02c7f75a3ecb59adbb7fd7ee30c3ef3cfcc3600
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-03-21 14:17:59 +0800

    branch detail -> no member plan warning

--- app/home/branch/[id]/page.js
+++ app/home/branch/[id]/page.js
@@ -1,4 +1,6 @@
 import $api from "@/app/_api";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import NoData from "@/app/_components/custom/NoData";
 import BranchDetailCard from "@/app/_components/home/BranchDetailCard";
 
@@ -12,14 +14,22 @@ export const generateMetadata = async ({ params }) => {
 };
 
 const BranchDetail = async ({ params }) => {
+  const cookieStore = cookies();
   const { id } = params;
   const { data: branch } = await $api.member.branch.getOne(id); // Fetching branch detail on the server side
+  const { status, data: memberPlan } = await $api.member.memberPlan.getMany(
+    cookieStore.get("token").value
+  );
+
+  if (status === 401) {
+    redirect("/auth/login");
+  }
 
   return (
     <>
       {branch ? (
         <>
-          <BranchDetailCard branch={branch} />
+          <BranchDetailCard branch={branch} memberPlan={memberPlan} />
         </>
       ) : (
         <>
