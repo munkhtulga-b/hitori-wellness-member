49accb505a07ad60573d1f04a43a8af517a8f7a2
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-04-18 15:36:16 +0800

    merge

--- app/_components/home/time-slot/TimeSlotSelect.js
+++ app/_components/home/time-slot/TimeSlotSelect.js
@@ -112,7 +112,10 @@ const TimeSlotSelect = ({ timeSlotList, fetchTimeslots, isFetching }) => {
           if (slotIndex !== -1) {
             daysOfWeek[item.week_day].timeSlots[slotIndex].currentCapacity =
               slot.current_capacity;
-            if (slot.current_capacity >= item.max_capacity) {
+            if (
+              slot.current_capacity >= item.max_capacity ||
+              !slot.is_available
+            ) {
               daysOfWeek[item.week_day].timeSlots[
                 slotIndex
               ].isAvailable = false;
