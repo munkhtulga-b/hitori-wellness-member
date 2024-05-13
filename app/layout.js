855c1a4a4f647b04cdb6888e9ac53112176cdc59
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-05-13 18:12:52 +0800

    gAnalytics script

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
@@ -25,6 +26,19 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B53SHX53M8"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B53SHX53M8');`}
+        </Script>
+      </head>
       <body className={fontFamily.className}>
         <AntdRegistry>
           <ToastContainer pauseOnFocusLoss={false} />
