4ecf733534bd721484d82f5dea01fb27c1b6c547
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-05-22 18:42:39 +0800

    merge

--- app/home/layout.js
+++ app/home/layout.js
@@ -1,8 +1,8 @@
 "use client";
 
 import NavigationBar from "../_components/home/NavigationBar";
-import { useLayoutEffect, useState, Suspense } from "react";
-import { redirect, useRouter, usePathname } from "next/navigation";
+import { useState, Suspense } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import { Layout } from "antd";
 import { useUserStore } from "../_store/user";
 import SideBar from "../_components/home/SideBar";
@@ -13,16 +13,7 @@ const { Header, Content, Sider } = Layout;
 const UserAuthenticatedLayout = ({ children }) => {
   const router = useRouter();
   const pathName = usePathname();
-  const [isMounted, setIsMounted] = useState(false);
-  const user = useUserStore((state) => state.user);
   const clearUser = useUserStore((state) => state.logOut);
-
-  useLayoutEffect(() => {
-    if (!user) {
-      redirect("/auth/login");
-    }
-    setIsMounted(true);
-  }, []);
 
   const logOut = () => {
     clearUser();
@@ -33,76 +24,69 @@ const UserAuthenticatedLayout = ({ children }) => {
 
   return (
     <div className="tw-flex tw-flex-col">
-      {isMounted ? (
-        <>
-          <Layout
+      <Layout
+        style={{
+          minHeight: "100svh",
+        }}
+      >
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          collapsedWidth={0}
+          trigger={null}
+          width={275}
+          style={{
+            position: "fixed",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: 10000,
+            backgroundColor: "#FFF",
+          }}
+        >
+          <SideBar setCollapsed={setCollapsed} onLogOut={logOut} />
+        </Sider>
+        <Layout>
+          <Header
             style={{
-              minHeight: "100svh",
+              padding: 0,
+              height: "auto",
+              position: "sticky",
+              top: 0,
+              zIndex: 999,
             }}
           >
-            <Sider
-              collapsible
-              collapsed={collapsed}
-              onCollapse={(value) => setCollapsed(value)}
-              collapsedWidth={0}
-              trigger={null}
-              width={275}
-              style={{
-                position: "fixed",
-                top: 0,
-                bottom: 0,
-                right: 0,
-                zIndex: 10000,
-                backgroundColor: "#FFF",
+            <NavigationBar setCollapsed={setCollapsed} collapsed={collapsed} />
+          </Header>
+          <Content
+            style={{
+              padding: "16px",
+              overflow: "hidden",
+            }}
+          >
+            {!collapsed && (
+              <div
+                onClick={() => setCollapsed(true)}
+                className="tw-fixed tw-top-0 tw-bottom-0 tw-left-0 tw-right-0 tw-bg-black/20 tw-z-[9999]"
+              ></div>
+            )}
+            <motion.div
+              key={pathName}
+              initial={{
+                opacity: 0,
               }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{ duration: 0.5 }}
+              className="tw-h-full"
             >
-              <SideBar setCollapsed={setCollapsed} onLogOut={logOut} />
-            </Sider>
-            <Layout>
-              <Header
-                style={{
-                  padding: 0,
-                  height: "auto",
-                  position: "sticky",
-                  top: 0,
-                  zIndex: 999,
-                }}
-              >
-                <NavigationBar
-                  setCollapsed={setCollapsed}
-                  collapsed={collapsed}
-                />
-              </Header>
-              <Content
-                style={{
-                  padding: "16px",
-                  overflow: "hidden",
-                }}
-              >
-                {!collapsed && (
-                  <div
-                    onClick={() => setCollapsed(true)}
-                    className="tw-fixed tw-top-0 tw-bottom-0 tw-left-0 tw-right-0 tw-bg-black/20 tw-z-[9999]"
-                  ></div>
-                )}
-                <motion.div
-                  key={pathName}
-                  initial={{
-                    opacity: 0,
-                  }}
-                  animate={{
-                    opacity: 1,
-                  }}
-                  transition={{ duration: 0.5 }}
-                  className="tw-h-full"
-                >
-                  <Suspense fallback={<></>}>{children}</Suspense>
-                </motion.div>
-              </Content>
-            </Layout>
-          </Layout>
-        </>
-      ) : null}
+              <Suspense fallback={<></>}>{children}</Suspense>
+            </motion.div>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 };
