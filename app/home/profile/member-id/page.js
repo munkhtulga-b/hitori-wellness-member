6796f74bc232b47043e684e1523fc67117288e6e
munkhtulga-mirai <munkhtulga@mirai.mn>
2024-07-03 13:01:29 +0800

    qr code warning description added

--- app/home/profile/member-id/page.js
+++ app/home/profile/member-id/page.js
@@ -8,6 +8,7 @@ import QRCode from "react-qr-code";
 
 const UserMemberId = () => {
   const [qrCode, setQrCode] = useState(null);
+  const [warningDesc, setWarningDesc] = useState(null);
 
   useEffect(() => {
     fetchQrCode();
@@ -17,6 +18,7 @@ const UserMemberId = () => {
     const { isOk, data } = await $api.member.remoteLock.getQR();
     if (isOk) {
       setQrCode(data?.booking?.universal_access_key_url || null);
+      setWarningDesc(data?.m_studio?.warning_desc || null);
     }
   };
 
@@ -26,30 +28,39 @@ const UserMemberId = () => {
         <section>
           <span className="tw-text-xxl tw-font-medium">会員ID</span>
         </section>
-        <section className="tw-absolute tw-top-1/2 tw-translate-y-[-50%]">
-          <div className="tw-flex tw-flex-col tw-gap-6 tw-items-center">
-            <section className="tw-size-[220px]">
-              {qrCode !== null && (
+        <section className="tw-absolute tw-top-1/2 tw-left-1/2 tw-translate-x-[-50%] tw-translate-y-[-50%]">
+          {qrCode !== null ? (
+            <div className="tw-flex tw-flex-col tw-gap-6 tw-items-center">
+              <section className="tw-size-[220px]">
                 <QRCode
                   size={256}
                   style={{ height: "auto", maxWidth: "100%", width: "100%" }}
                   value={qrCode}
                   viewBox={`0 0 256 256`}
                 />
-              )}
-              {/* <Image
+                {/* <Image
                 src={`data:image/png;base64,${base64string}`}
                 alt="qr-code"
                 width={250}
                 height={250}
               /> */}
-            </section>
+              </section>
+              {warningDesc ? (
+                <section className="tw-px-8">
+                  <p
+                    dangerouslySetInnerHTML={{ __html: warningDesc }}
+                    className="tw-leading-[22px] tw-tracking-[0.14px]"
+                  ></p>
+                </section>
+              ) : null}
+            </div>
+          ) : (
             <section className="tw-px-8">
-              <p>
-                入店の際にはこの会員QRコードを店舗端末にかざしてください。またはピンコードの入力で入店も可能です。
+              <p className="tw-leading-[22px] tw-tracking-[0.14px]">
+                QRコード・ピンコードは予約が発生した場合のみ開始時間の10分前から表示されます。
               </p>
             </section>
-          </div>
+          )}
         </section>
       </div>
     </>
